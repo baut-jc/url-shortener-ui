feab15b39eb8602300f804388baf392ef4cc42df
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-07 16:05:45 -0500

    Test submitted inputs to display

--- cypress/e2e/UrlShortener.cy.js
+++ cypress/e2e/UrlShortener.cy.js
@@ -1,6 +1,7 @@
 describe('URL Shortener', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', 'http://localhost:3000/api/v1/urls', {fixture: 'example.json'})
+    cy.visit('http://localhost:3000')
   })
 
   it('should display the page title and existing shortened URLs', () => {
@@ -17,5 +18,18 @@ describe('URL Shortener', () => {
   it('should reflect information in the input field', () => {
     cy.get('[placeholder="Title..."]').type('Title here')
     cy.get('[placeholder="URL to Shorten..."]').type('superloooooongURLandlotsofslash-and-dash.com')
+  })
+
+  it('should show the submitted inputs', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      id: 12,
+      long_url:
+        'superloooooongURLandlotsofslash-and-dash.com',
+      short_url: 'http://localhost:3001/useshorturl/12',
+      title: 'Title here',
+    }).as('stub')
+    cy.get('button').click()
+    cy.wait('@stub')
+    cy.get('h3').contains('Title here')
   })
 })
